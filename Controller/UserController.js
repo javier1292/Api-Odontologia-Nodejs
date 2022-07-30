d46aa428043fed74218db96c26e5415f2f9c52ca
javier1292 <66648525+javier1292@users.noreply.github.com>
2022-07-30 18:13:26 -0400

    controlador user completado

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -3,6 +3,8 @@ var validator = require("validator");
 var User = require("../model/user");
 var bcrypt = require("bcrypt-nodejs");
 var jwt = require("../services/jwt");
+var fs = require("fs");
+var path = require("path");
 
 var controller = {
   save: function (req, res) {
@@ -135,75 +137,171 @@ var controller = {
       });
     });
   },
-  update: function(req, res){
+  update: function (req, res) {
     //recoger los datos del usuari
     var params = req.body;
     //vaqlidar datos
     try {
-
       var validate_name = !validator.isEmpty(params.name);
       var validate_surname = !validator.isEmpty(params.surname);
       var validate_email =
         !validator.isEmpty(params.email) && validator.isEmail(params.email);
-     
-    }catch(err){
+    } catch (err) {
       res.status(400).send({
         message: "faltan datos por enviar ",
-        params
-      });
-    };
-
-    //eliminar propiedades innecesarias 
-    delete params.password;
-    //user id  
-    var userId = req.user.sub;
-
-    //comprobar el email 
-    if(req.user.email != params.email){
-      User.findOne({email:params.email.toLowerCase()},(err,user)=>{
-        if(err){
-          res.status(500).send({
-            message: "error al intentar identificarse",
-            
-          });
-        }
-  
-        if(user && user.email == params.email){
-          res.status(200).send({
-            message: "el email no puede ser modificado",
-            
-          });
-        }
-
-      });
-    }else{
-
-      //buscar y actualizar documentos de la base de datos 
-      User.findOneAndUpdate({_id: userId},params,{new:true},(err,userupdated)=>{
-  
-        if(err){
-          res.status(500).send({
-            message: "error al actualizar el user",
-            user: userupdated
-          });
-        }
-  
-        if(!userupdated){
-          res.status(500).send({
-            message: "error",
-            user: userupdated
-          });
-        }
-        //devolver respusta
-        res.status(200).send({
-          message: "update user",
-          user: userupdated
-        });
-  
+        params,
       });
     }
 
+    //eliminar propiedades innecesarias
+    delete params.password;
+    //user id
+    var userId = req.user.sub;
 
+    //comprobar el email
+    if (req.user.email != params.email) {
+      User.findOne({ email: params.email.toLowerCase() }, (err, user) => {
+        if (err) {
+          res.status(500).send({
+            message: "error al intentar identificarse",
+          });
+        }
+
+        if (user && user.email == params.email) {
+          res.status(200).send({
+            message: "el email no puede ser modificado",
+          });
+        }
+      });
+    } else {
+      //buscar y actualizar documentos de la base de datos
+      User.findOneAndUpdate(
+        { _id: userId },
+        params,
+        { new: true },
+        (err, userupdated) => {
+          if (err) {
+            res.status(500).send({
+              message: "error al actualizar el user",
+              user: userupdated,
+            });
+          }
+
+          if (!userupdated) {
+            res.status(500).send({
+              message: "error",
+              user: userupdated,
+            });
+          }
+          //devolver respusta
+          res.status(200).send({
+            message: "update user",
+            user: userupdated,
+          });
+        }
+      );
+    }
+  },
+  upload: function (req, res) {
+    //recoger el fichero de la peticion
+    var filename = "avatar no subido.....";
+
+    if (!req.files) {
+      return res.status(404).send({
+        status: "error",
+        message: filename,
+      });
+    }
+
+    //conseguir el nombre y la extencion del archivo subido
+    var file_path = req.files.file0.path;
+    var file_split = file_path.split("\\");
+    //nombre del archivo
+    var file_name = file_split[2];
+    //extencion del archivo
+    var ext_split = file_name.split(".");
+    var file_ext = ext_split[1];
+    //comprobar extension
+    if (
+      file_ext != "png" &&
+      file_ext != "jpg" &&
+      file_ext != "jpeg" &&
+      file_ext != "gif"
+    ) {
+      fs.unlink(file_path, (err) => {
+        return res.status(400).send({
+          message: "la extencion del archivo no es valida",
+        });
+      });
+    } else {
+      // sacar el usuario identificado
+      var userId = req.user.sub;
+      //hacer el update para actualizar el opbjeto
+      User.findByIdAndUpdate(
+        { _id: userId },
+        { image: file_name },
+        { new: true },
+        (err, userUpdate) => {
+          if (err || !userUpdate) {
+            return res.status(500).send({
+              message: "error al subir imagen ",
+            });
+          } else {
+            //devolver una respusta
+            return res.status(200).send({
+              message: "uploaded",
+              user: userUpdate,
+            });
+          }
+        }
+      );
+    }
+
+    //comprobar el usuario identificado
+  },
+  avatar: function (req, res) {
+    var fileName = req.params.fileName;
+    var pathfile = "./uploads/users/" + fileName;
+    console.log(pathfile);
+
+    if (fs.existsSync(pathfile)) {
+      return res.sendFile(path.resolve(pathfile));
+    }else{
+      return res.status(404).send({
+        message: "la imagen no existe",
+      });
+    }
+  },
+  getusers: function(req, res){
+    User.find().exec((err,users)=>{
+      if (err || !users){
+        return res.status(404).send({
+          message: "Usuarios no encontrados"
+        });
+      }else{
+        return res.status(200).send({
+          status: "success",
+          users
+        });
+      }
+    });
+
+  },
+  getuser: function(req,res){
+    var userId = req.params.userId;
+    User.findById(userId).exec((err,user)=>{
+      if (err || !user){
+        return res.status(404).send({
+          message: "Usuarios no encontrados"
+        });
+      }else{
+        return res.status(200).send({
+          status: "success",
+          user
+        });
+      }
+
+    });
   }
 };
 
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,11 +4,17 @@ var express = require('express');
 var UserController = require('../Controller/UserController');
 
 var routes = express.Router();
-var md_auth = require('../middleware/authenticate')
+var md_auth = require('../middleware/authenticate');
+var multiparty = require('connect-multiparty');
+var md_uploaad = multiparty({uploadDir: './uploads/users'})
 
 //rustas de usuarios 
 routes.post('/register',UserController.save);
 routes.post('/login',UserController.login);
+routes.post('/uploadAvatar',[md_uploaad, md_auth.authenticate],UserController.upload);
 routes.put('/user/update',md_auth.authenticate,UserController.update);
-
+routes.get('/avatar/:fileName',UserController.avatar);
+routes.get('/users',UserController.getusers);
+routes.get('/user/:userId',UserController.getuser);
+ 
 module.exports = routes;
