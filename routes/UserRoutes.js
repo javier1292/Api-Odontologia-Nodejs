234fbd65328615932c9e9330ae9ed0625c4a6096
javier1292 <66648525+javier1292@users.noreply.github.com>
2022-07-30 15:02:56 -0400

    middleware de authentificacion

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,10 +4,11 @@ var express = require('express');
 var UserController = require('../Controller/UserController');
 
 var routes = express.Router();
+var md_auth = require('../middleware/authenticate')
 
 //rustas de usuarios 
 routes.post('/register',UserController.save);
 routes.post('/login',UserController.login);
-routes.put('/user/update',UserController.update);
+routes.put('/user/update',md_auth.authenticate,UserController.update);
 
 module.exports = routes;
